5b5416e5fdac8afe45a3ae8900b923b7055a3970
hiro <hiro.sec@outlook.jp>
2026-06-13 10:22:29 +0900

    fix: enable treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,22 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
+    config = function()
+      -- パーサーのインストール
+      require("nvim-treesitter").install({
         "lua", "rust", "typescript", "tsx",
         "cpp", "c", "python", "javascript",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
+      })
+
+      -- ハイライトの有効化
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+    end,
   },
 }
